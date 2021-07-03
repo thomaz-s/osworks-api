cd51902f93f7e1634b1b4d6ae42c3991aa44a306
thomaz <thomazsousa@outlook.com.br>
2021-07-03 13:37:13 -0300

    enviar pra github

---
+++ src/main/java/com/algaworks/osworks/domain/model/Cliente.java
@@ -0,0 +1,106 @@
+package com.algaworks.osworks.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Cliente{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max=60)
+    private String nome;
+    
+    @NotBlank
+    @Email
+    @Size(max=255)
+    private String email;
+    
+    @NotBlank
+    @Size(max=20)
+    private String telefone;
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/algaworks/osworks/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.algaworks.osworks.domain.repository;
+
+import com.algaworks.osworks.domain.model.Cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+    Cliente findByEmail(String email);
+}
